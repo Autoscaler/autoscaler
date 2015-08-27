fb868d49421a4037eabe6edf3dcd4dd7a0453b14
Richard D.G. Hickman <richard.hickman@hp.com>
2015-08-27 16:05:04 +0100

    Initial commit for autoscale-marathon-shared

---
+++ src/main/java/com/hp/caf/autoscale/MarathonAutoscaleConfiguration.java
@@ -0,0 +1,52 @@
+package com.hp.caf.autoscale;
+
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+/**
+ * Shared configuration between the MarathonServiceScaler and MarathonServiceSource.
+ */
+public class MarathonAutoscaleConfiguration
+{
+    /**
+     * The URL to the Marathon HTTP server.
+     */
+    @NotNull
+    @Size(min = 1)
+    private String endpoint;
+    /**
+     * The absolute maximum instances for any service in Marathon.
+     */
+    @Min(1)
+    private int maximumInstances;
+
+
+    public MarathonAutoscaleConfiguration() { }
+
+
+    public String getEndpoint()
+    {
+        return endpoint;
+    }
+
+
+    public void setEndpoint(final String endpoint)
+    {
+        this.endpoint = endpoint;
+    }
+
+
+    public int getMaximumInstances()
+    {
+        return maximumInstances;
+    }
+
+
+    public void setMaximumInstances(final int maximumInstances)
+    {
+        this.maximumInstances = maximumInstances;
+    }
+}

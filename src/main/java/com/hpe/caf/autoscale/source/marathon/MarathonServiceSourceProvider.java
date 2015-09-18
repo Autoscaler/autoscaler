1598b81ae6f12ca99927531b09ccb5dd873070f4
Richard D.G. Hickman <richard.hickman@hp.com>
2015-09-18 11:42:48 +0100

    Set HTTP timeout to 10 seconds

--- src/main/java/com/hpe/caf/autoscale/source/marathon/MarathonServiceSourceProvider.java
+++ src/main/java/com/hpe/caf/autoscale/source/marathon/MarathonServiceSourceProvider.java
@@ -8,6 +8,8 @@ import com.hpe.caf.api.autoscale.ScalerException;
 import com.hpe.caf.api.autoscale.ServiceSource;
 import com.hpe.caf.api.autoscale.ServiceSourceProvider;
 import com.hpe.caf.autoscale.MarathonAutoscaleConfiguration;
+import feign.Feign;
+import feign.Request;
 import mesosphere.marathon.client.Marathon;
 import mesosphere.marathon.client.MarathonClient;
 
@@ -18,6 +20,9 @@ import java.util.Iterator;
 
 public class MarathonServiceSourceProvider implements ServiceSourceProvider
 {
+    private static final int MARATHON_TIMEOUT = 10_000;
+
+
     @Override
     public ServiceSource getServiceSource(final ConfigurationSource configurationSource, final ServicePath servicePath)
             throws ScalerException
@@ -29,7 +34,8 @@ public class MarathonServiceSourceProvider implements ServiceSourceProvider
                 groupPath.append(groupIterator.next()).append('/');
             }
             MarathonAutoscaleConfiguration config = configurationSource.getConfiguration(MarathonAutoscaleConfiguration.class);
-            Marathon marathon = MarathonClient.getInstance(config.getEndpoint());
+            Feign.Builder builder = Feign.builder().options(new Request.Options(MARATHON_TIMEOUT, MARATHON_TIMEOUT));
+            Marathon marathon = MarathonClient.getInstance(builder, config.getEndpoint());
             return new MarathonServiceSource(marathon, groupPath.toString(), new URL(config.getEndpoint()));
         } catch (ConfigurationException | MalformedURLException e) {
             throw new ScalerException("Failed to create service source", e);

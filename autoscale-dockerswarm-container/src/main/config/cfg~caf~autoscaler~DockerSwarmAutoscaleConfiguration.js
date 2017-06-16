7a2487267eb04831695d898f8d204ca23cde7c34
Trevor Getty <trevor.getty@hpe.com>
2017-06-16 19:21:37 +0100

    update to help get rid of js warnings.

--- autoscale-dockerswarm-container/src/main/config/cfg~caf~autoscaler~DockerSwarmAutoscaleConfiguration.js
+++ autoscale-dockerswarm-container/src/main/config/cfg~caf~autoscaler~DockerSwarmAutoscaleConfiguration.js
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*global getenv */
 ({
     endpoint: getenv("DOCKER_HOST")
             || ("unix:///var/run/docker.sock"),
--- autoscale-source-dockerswarm/src/test/java/com/hpe/caf/autoscale/source/docker/swarm/DeveloperEndpointIT.java
+++ autoscale-source-dockerswarm/src/test/java/com/hpe/caf/autoscale/source/docker/swarm/DeveloperEndpointIT.java
@@ -40,7 +40,6 @@ public class DeveloperEndpointIT
     {
         DockerSwarmAutoscaleConfiguration config = new DockerSwarmAutoscaleConfiguration();
         config.setEndpoint("http://192.168.56.10:2375");
-        config.setProxyEndpoint("http://getty5:8888");
         config.setTimeoutInSecs(new Long(10));
         config.setStackId("jobservicedemo");
         return config;

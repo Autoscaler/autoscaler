b97d05033fb378b80dfa4e1b8883633cd162f402
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-12-18 19:47:10 +0000

    code tidy

--- autoscale-core/src/main/java/com/hpe/caf/autoscale/core/GovernorImpl.java
+++ autoscale-core/src/main/java/com/hpe/caf/autoscale/core/GovernorImpl.java
@@ -79,20 +79,19 @@ public class GovernorImpl implements Governor {
                 if (lastInstanceInfo.getTotalInstances() > scalingConfiguration.getMaxInstances()) {
                     return new ScalingAction(ScalingOperation.SCALE_DOWN,
                                              lastInstanceInfo.getTotalInstances() - scalingConfiguration.getMaxInstances());
-                }
-                if(!otherServicesMinimumInstancesMet){ 
+                } else if (!otherServicesMinimumInstancesMet) {
                     if (lastInstanceInfo.getTotalInstances() < scalingConfiguration.getMinInstances()) {
                         return new ScalingAction(ScalingOperation.SCALE_UP,
                                                  scalingConfiguration.getMinInstances() - lastInstanceInfo.getTotalInstances());
                     } else if (lastInstanceInfo.getTotalInstances() == scalingConfiguration.getMinInstances()) {
                         return new ScalingAction(ScalingOperation.NONE, 0);
-                    }
-                    else if(lastInstanceInfo.getTotalInstances()>scalingConfiguration.getMinInstances()){
+                    } else if (lastInstanceInfo.getTotalInstances() > scalingConfiguration.getMinInstances()) {
                         //Gradually reduce the totalInstances by a percentage until Minimums are met.
                         //This should be configurable, however there should be a Governor specific configuration
                         //to allow different Governor implementations to be added without polluting the AutoscaleConfiguration
 
-                        int target = Math.max(scalingConfiguration.getMinInstances(), (int)Math.floor(lastInstanceInfo.getTotalInstances() * reduceToPercentage));
+                        int target = Math.max(scalingConfiguration.getMinInstances(),
+                                              (int) Math.floor(lastInstanceInfo.getTotalInstances() * reduceToPercentage));
                         int amount = lastInstanceInfo.getTotalInstances() - target;
 
                         return new ScalingAction(ScalingOperation.SCALE_DOWN, amount);

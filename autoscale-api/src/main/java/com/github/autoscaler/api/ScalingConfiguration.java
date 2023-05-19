708a8176595e8491cbc45ce174f832ad7fc1b21f
Rory <rory.torney@microfocus.com>
2023-05-19 13:04:35 +0100

    Fix label

--- autoscale-api/src/main/java/com/github/autoscaler/api/ScalingConfiguration.java
+++ autoscale-api/src/main/java/com/github/autoscaler/api/ScalingConfiguration.java
@@ -37,7 +37,7 @@ public final class ScalingConfiguration
     public static final String KEY_BACKOFF_AMOUNT = "autoscale.backoff";
     public static final String KEY_SCALE_DOWN_BACKOFF_AMOUNT = "autoscale.scaledownbackoff";
     public static final String KEY_SCALE_UP_BACKOFF_AMOUNT = "autoscale.scaleupbackoff";
-    public static final String KEY_SHUTDOWN_PRIORITY = "autoscale.shutdownPriority";
+    public static final String KEY_SHUTDOWN_PRIORITY = "autoscale.shutdownpriority";
     public static final String KEY_GROUP_ID = "autoscale.groupid";
     /**
      * The unique id of the service

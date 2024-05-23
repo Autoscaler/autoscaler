57feda5b59bec85fa280c0995a22c52bcf73e860
caoimhelynch <59800744+caoimhelynch@users.noreply.github.com>
2024-05-23 09:49:58 +0100

    US914106: Version Currency: Replace PowerMock (#150)
    * US914106: Version Currency: Replace PowerMock
    
    * tidy
    
    * Use mock constructor rather than factory
    
    * accidental semicolon
    
    * formatting

--- autoscale-scaler-kubernetes/src/test/java/com/github/autoscaler/scaler/kubernetes/K8sHealthCheckTest.java
+++ autoscale-scaler-kubernetes/src/test/java/com/github/autoscaler/scaler/kubernetes/K8sHealthCheckTest.java
@@ -26,141 +26,145 @@ import io.kubernetes.client.openapi.models.*;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ Kubectl.class, K8sHealthCheck.class })
-@PowerMockIgnore({"jdk.internal.reflect.*", "javax.net.ssl.*"})
+@RunWith(MockitoJUnitRunner.class)
 public class K8sHealthCheckTest {
 
     @Test
     public void testHealthCheck_ReturnHealthy() throws Exception {
-        final KubectlVersion version = Mockito.mock(KubectlVersion.class);
-        final VersionInfo info = Mockito.mock(VersionInfo.class);
-        PowerMockito.mockStatic(Kubectl.class);
-        when(Kubectl.version()).thenReturn(version);
-        when(Kubectl.version().execute()).thenReturn(info);
 
         final V1SubjectAccessReviewStatus status = new V1SubjectAccessReviewStatus();
         status.setAllowed(true);
         final V1SelfSubjectAccessReview review = new V1SelfSubjectAccessReview();
         review.setStatus(status);
 
-        final V1SelfSubjectAccessReview body = PowerMockito.mock(V1SelfSubjectAccessReview.class);
-        PowerMockito.whenNew(V1SelfSubjectAccessReview.class)
-                .withNoArguments().thenReturn(body);
-
-        final AuthorizationV1Api authApi = PowerMockito.mock(AuthorizationV1Api.class);
-        PowerMockito.whenNew(AuthorizationV1Api.class)
-                .withNoArguments().thenReturn(authApi);
         final AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest requestBuilder
-            = PowerMockito.mock(AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest.class);
-        when(authApi.createSelfSubjectAccessReview(body)).thenReturn(requestBuilder);
-        when(requestBuilder.dryRun("All")).thenReturn(requestBuilder);
-        when(requestBuilder.fieldManager(null)).thenReturn(requestBuilder);
-        when(requestBuilder.fieldValidation(null)).thenReturn(requestBuilder);
-        when(requestBuilder.pretty("true")).thenReturn(requestBuilder);
-        when(requestBuilder.execute()).thenReturn(review);
+                = Mockito.mock(AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest.class);
 
-        final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
-        final List<String> mockNamespaces = Collections.singletonList("private");
-        when(config.getNamespacesArray()).thenReturn(mockNamespaces);
-        final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
+        try (MockedConstruction<V1SelfSubjectAccessReview> mockV1SelfSubjectAccessReview =
+                     Mockito.mockConstruction(V1SelfSubjectAccessReview.class);
+             MockedConstruction<AuthorizationV1Api> mockAuthorizationV1Api =
+                     Mockito.mockConstruction(AuthorizationV1Api.class, (mock, context) ->{
+                         when(mock.createSelfSubjectAccessReview(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).dryRun(anyString())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).fieldManager(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).fieldValidation(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).pretty(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).execute()).thenReturn(review);
+                     });
+             MockedStatic<Kubectl> mockKubectl = Mockito.mockStatic(Kubectl.class)
+             ) {
+            final KubectlVersion version = Mockito.mock(KubectlVersion.class);
+            final VersionInfo info = Mockito.mock(VersionInfo.class);
+            when(Kubectl.version()).thenReturn(version);
+            when(Kubectl.version().execute()).thenReturn(info);
 
-        assertEquals(HealthResult.RESULT_HEALTHY, serviceScaler.healthCheck());
+            final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
+            final List<String> mockNamespaces = Collections.singletonList("private");
+            when(config.getNamespacesArray()).thenReturn(mockNamespaces);
+            final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
+
+            assertEquals(HealthResult.RESULT_HEALTHY, serviceScaler.healthCheck());
+        }
     }
 
     @Test
     public void testHealthCheck_ReturnUnhealthyConnectionError() throws Exception {
-        final KubectlVersion version = Mockito.mock(KubectlVersion.class);
-        PowerMockito.mockStatic(Kubectl.class);
-        when(Kubectl.version()).thenReturn(version);
-        when(Kubectl.version().execute()).thenThrow(new KubectlException("Error connecting to Kubernetes"));
 
-        final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
-        final List<String> mockNamespaces = Collections.singletonList("private");
-        when(config.getNamespacesArray()).thenReturn(mockNamespaces);
-        final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
+        try(MockedStatic<Kubectl> mockKubectl = Mockito.mockStatic(Kubectl.class)){
+            final KubectlVersion version = Mockito.mock(KubectlVersion.class);
+            when(Kubectl.version()).thenReturn(version);
+            when(Kubectl.version().execute()).thenThrow(new KubectlException("Error connecting to Kubernetes"));
 
-        final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY,
-                "Cannot connect to Kubernetes");
-        final HealthResult actualResult = serviceScaler.healthCheck();
+            final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
+            final List<String> mockNamespaces = Collections.singletonList("private");
+            final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
 
-        assertEquals(expectedResult.getStatus(), actualResult.getStatus());
-        assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+            final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY,
+                    "Cannot connect to Kubernetes");
+            final HealthResult actualResult = serviceScaler.healthCheck();
+
+            assertEquals(expectedResult.getStatus(), actualResult.getStatus());
+            assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+        }
     }
 
     @Test
     public void testHealthCheck_ReturnUnhealthyPermissionError() throws Exception {
-        final KubectlVersion version = Mockito.mock(KubectlVersion.class);
-        final VersionInfo info = Mockito.mock(VersionInfo.class);
-        PowerMockito.mockStatic(Kubectl.class);
-        when(Kubectl.version()).thenReturn(version);
-        when(Kubectl.version().execute()).thenReturn(info);
 
         final V1SubjectAccessReviewStatus status = new V1SubjectAccessReviewStatus();
         status.setAllowed(false);
         final V1SelfSubjectAccessReview review = new V1SelfSubjectAccessReview();
         review.setStatus(status);
 
-        final V1SelfSubjectAccessReview body = PowerMockito.mock(V1SelfSubjectAccessReview.class);
-        PowerMockito.whenNew(V1SelfSubjectAccessReview.class)
-                .withNoArguments().thenReturn(body);
-
-        final AuthorizationV1Api authApi = PowerMockito.mock(AuthorizationV1Api.class);
-        PowerMockito.whenNew(AuthorizationV1Api.class)
-                .withNoArguments().thenReturn(authApi);
         final AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest requestBuilder
-            = PowerMockito.mock(AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest.class);
-        when(authApi.createSelfSubjectAccessReview(body)).thenReturn(requestBuilder);
-        when(requestBuilder.dryRun("All")).thenReturn(requestBuilder);
-        when(requestBuilder.fieldManager(null)).thenReturn(requestBuilder);
-        when(requestBuilder.fieldValidation(null)).thenReturn(requestBuilder);
-        when(requestBuilder.pretty("true")).thenReturn(requestBuilder);
-        when(requestBuilder.execute()).thenReturn(review);
+                = Mockito.mock(AuthorizationV1Api.APIcreateSelfSubjectAccessReviewRequest.class);
 
-        final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
-        final List<String> mockNamespaces = Collections.singletonList("private");
-        when(config.getNamespacesArray()).thenReturn(mockNamespaces);
-        final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
+        try (MockedConstruction<V1SelfSubjectAccessReview> mockV1SelfSubjectAccessReview =
+                     Mockito.mockConstruction(V1SelfSubjectAccessReview.class);
+             MockedConstruction<AuthorizationV1Api> mockAuthorizationV1Api =
+                     Mockito.mockConstruction(AuthorizationV1Api.class, (mock, context) ->{
+                         when(mock.createSelfSubjectAccessReview(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).dryRun(anyString())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).fieldManager(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).fieldValidation(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).pretty(any())).thenReturn(requestBuilder);
+                         when(mock.createSelfSubjectAccessReview(any()).execute()).thenReturn(review);
+                     });
+             MockedStatic<Kubectl> mockKubectl = Mockito.mockStatic(Kubectl.class)
+        ) {
+            final KubectlVersion version = Mockito.mock(KubectlVersion.class);
+            final VersionInfo info = Mockito.mock(VersionInfo.class);
+            when(Kubectl.version()).thenReturn(version);
+            when(Kubectl.version().execute()).thenReturn(info);
 
-        final String expectedMessage = String.format("Error: Kubernetes Service Account does not have correct permissions: %s", StringUtils.normalizeSpace(review.toString()));
+            final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
+            final List<String> mockNamespaces = Collections.singletonList("private");
+            when(config.getNamespacesArray()).thenReturn(mockNamespaces);
+            final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
 
-        final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY, expectedMessage);
-        final HealthResult actualResult = serviceScaler.healthCheck();
+            final String expectedMessage = String.format("Error: Kubernetes Service Account does not have correct permissions: %s",
+                    StringUtils.normalizeSpace(review.toString()));
 
-        assertEquals(expectedResult.getStatus(), actualResult.getStatus());
-        assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+            final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY, expectedMessage);
+            final HealthResult actualResult = serviceScaler.healthCheck();
+
+            assertEquals(expectedResult.getStatus(), actualResult.getStatus());
+            assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+        }
     }
 
     @Test
     public void testHealthCheck_ReturnNoNamespacesError() throws Exception
     {
-        final KubectlVersion version = Mockito.mock(KubectlVersion.class);
-        final VersionInfo info = Mockito.mock(VersionInfo.class);
-        PowerMockito.mockStatic(Kubectl.class);
-        when(Kubectl.version()).thenReturn(version);
-        when(Kubectl.version().execute()).thenReturn(info);
+        try(MockedStatic<Kubectl> mockKubectl = Mockito.mockStatic(Kubectl.class)) {
+            final KubectlVersion version = Mockito.mock(KubectlVersion.class);
+            final VersionInfo info = Mockito.mock(VersionInfo.class);
+            when(Kubectl.version()).thenReturn(version);
+            when(Kubectl.version().execute()).thenReturn(info);
 
-        final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
-        final List<String> mockNamespaces = Collections.emptyList();
-        when(config.getNamespacesArray()).thenReturn(mockNamespaces);
-        final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
+            final K8sAutoscaleConfiguration config = Mockito.mock(K8sAutoscaleConfiguration.class);
+            final List<String> mockNamespaces = Collections.emptyList();
+            when(config.getNamespacesArray()).thenReturn(mockNamespaces);
+            final K8sServiceScaler serviceScaler = new K8sServiceScaler(config);
 
-        final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY, "Error: No namespaces were found");
-        final HealthResult actualResult = serviceScaler.healthCheck();
+            final HealthResult expectedResult = new HealthResult(HealthStatus.UNHEALTHY, "Error: No namespaces were found");
+            final HealthResult actualResult = serviceScaler.healthCheck();
 
-        assertEquals(expectedResult.getStatus(), actualResult.getStatus());
-        assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+            assertEquals(expectedResult.getStatus(), actualResult.getStatus());
+            assertEquals(expectedResult.getMessage(), actualResult.getMessage());
+        }
     }
 }

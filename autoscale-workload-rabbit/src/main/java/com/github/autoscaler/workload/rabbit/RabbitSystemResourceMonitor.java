085dd1d214534214a563a90eb2672580e28cb8ef
David Milligan <dmilligan@opentext.com>
2025-09-08 09:25:11 +0100

    Reverted change

--- autoscale-workload-rabbit/src/main/java/com/github/autoscaler/workload/rabbit/RabbitSystemResourceMonitor.java
+++ autoscale-workload-rabbit/src/main/java/com/github/autoscaler/workload/rabbit/RabbitSystemResourceMonitor.java
@@ -88,7 +88,8 @@ public final class RabbitSystemResourceMonitor
         return new ResourceUtilisation(memoryAllocated, diskFreeMbOpt);
     }
 
-    private boolean shouldIssueRequest() {
+    private boolean shouldIssueRequest()
+    {
         if (lastTime == 0) {
             return true;
         }

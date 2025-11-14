9f9991c9517d63bbb8de991dbbea8c1357a0fcff
David Milligan <dmilligan@opentext.com>
2025-11-14 08:57:09 +0000

    Add missed import

--- autoscale-workload-rabbit/src/main/java/com/github/autoscaler/workload/rabbit/RabbitSystemResourceMonitor.java
+++ autoscale-workload-rabbit/src/main/java/com/github/autoscaler/workload/rabbit/RabbitSystemResourceMonitor.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Optional;
